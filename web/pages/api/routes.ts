cc5911e69c55382e4b0fea779c6885b878c7d71f
Vishal Arora <aroravishal629@gmail.com>
2021-12-25 22:01:40 +0530

    fix: Reverting hostCms route

--- web/pages/api/routes.ts
+++ web/pages/api/routes.ts
@@ -1,5 +1,5 @@
-export const hostCms = 'http://localhost:1337'
-// export const hostCms = 'https://grilling-ways-api.herokuapp.com'
+// export const hostCms = 'http://localhost:1337'
+export const hostCms = 'https://grilling-ways-api.herokuapp.com'
 
 export const routes = {
     posts: `${hostCms}/api/posts?populate=*`,
